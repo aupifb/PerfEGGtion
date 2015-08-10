b5bf403af198e0d40767cd0335691d9956750a90
aupifb <vincent.boudier@gmail.com>
2015-08-10 14:19:22 -0500

    early cardview

--- app/src/main/java/me/aupifb/perfeggtion/TabFragment2.java
+++ app/src/main/java/me/aupifb/perfeggtion/TabFragment2.java
@@ -3,15 +3,24 @@ package me.aupifb.perfeggtion;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TabFragment2 extends android.support.v4.app.Fragment {
+public class TabFragment2 extends android.support.v4.app.Fragment implements View.OnClickListener {
+
+    @Bind(R.id.alacoque)
+    ImageButton alacoque;
 
 
     public TabFragment2() {
@@ -22,9 +31,26 @@ public class TabFragment2 extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
+
+        View view2 = inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
+        setRetainInstance(true);
+
+        ButterKnife.bind(this, view2);
+        //Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        return (view2);
     }
 
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    @OnClick(R.id.alacoque)
+    public void dothis() {
+        Log.d("lol", "dothis ");
+        ((MainActivity) getActivity()).countdownstart(180);
+    }
 
 }
--- app/src/main/java/me/aupifb/perfeggtion/TimerConflictDialogFragment.java
+++ app/src/main/java/me/aupifb/perfeggtion/TimerConflictDialogFragment.java
@@ -26,7 +26,7 @@ public class TimerConflictDialogFragment extends DialogFragment {
         int title = getArguments().getInt("title");
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Stop currently running timer in order to start a new one").setTitle(title)
+        builder.setMessage(R.string.timer_conflict).setTitle(title)
                 .setPositiveButton(R.string.alert_dialog_ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         ((MainActivity) getActivity()).stoptimer();
