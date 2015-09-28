571682514faadce86018d5ee46dfbc541df70bac
aupifb <vincent.boudier@gmail.com>
2015-09-28 17:21:49 +0200

    add recipekitchen

---
+++ app/src/main/java/me/aupifb/perfeggtion/RecipeKitchen.java
@@ -0,0 +1,47 @@
+package me.aupifb.perfeggtion;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by aupifb on 28/09/2015.
+ */
+public class RecipeKitchen {
+
+    private static RecipeKitchen sRecipeKitchen;
+
+    private ArrayList<Recipe> mRecipes;
+
+    private RecipeKitchen(Context context) {
+        mRecipes = new ArrayList<>();
+    }
+
+    private static RecipeKitchen get(Context context) {
+        if (sRecipeKitchen == null) {
+            sRecipeKitchen = new RecipeKitchen(context);
+        }
+        return sRecipeKitchen;
+    }
+
+    public void addRecipe(Recipe r) {
+        mRecipes.add(r);
+    }
+
+    public List<Recipe> getRecipes() {
+        return mRecipes;
+    }
+
+    public Recipe getRecipe(UUID id) {
+        for (Recipe recipe : mRecipes) {
+            if (recipe.getId().equals(id)) {
+                return recipe;
+            }
+        }
+        return null;
+    }
+
+
+}
