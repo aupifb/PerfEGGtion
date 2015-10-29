20a3bf1d58726a898e17960f35879d05e0d59eba
aupifb <vincent.boudier@gmail.com>
2015-10-29 19:06:14 +0000

    xffv

--- app/src/main/java/me/aupifb/perfeggtion/ZoomFragment.java
+++ app/src/main/java/me/aupifb/perfeggtion/ZoomFragment.java
@@ -1,20 +1,28 @@
 package me.aupifb.perfeggtion;
 
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.signature.StringSignature;
 
 import java.io.File;
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,10 +30,12 @@ import java.io.File;
 public class ZoomFragment extends DialogFragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
-    private File mPhotoFile;
-    private ImageView mGlideZoomView;
+    private static final int REQUEST_PHOTO = 2;
     private Recipe mRecipe;
     private ImageView mGlideView;
+    private Button mButtonDelete;
+    private File mPhotoFile;
+    private ImageButton mPhotoButton;
 
     public ZoomFragment() {
         // Required empty public constructor
@@ -59,6 +69,26 @@ public class ZoomFragment extends DialogFragment {
         mGlideView = (ImageView) v.findViewById(R.id.zoom_photo);
         Glide.with(this).load(mPhotoFile).signature(new StringSignature(mRecipe.getSignature())).into(mGlideView);
 
+        mPhotoButton = (ImageButton) v.findViewById(R.id.recipe_camera);
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        PackageManager packageManager = getActivity().getPackageManager();
+        boolean canTakePhoto = mPhotoFile != null &&
+                captureImage.resolveActivity(packageManager) != null;
+        mPhotoButton.setEnabled(canTakePhoto);
+
+        if (canTakePhoto) {
+            Uri uri = Uri.fromFile(mPhotoFile);
+            captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        }
+
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(captureImage, REQUEST_PHOTO);
+            }
+        });
+
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setPositiveButton(android.R.string.ok,
@@ -75,7 +105,24 @@ public class ZoomFragment extends DialogFragment {
                         }
                 )
                 .create();
+
     }
 
+    private void updatePhotoView() {
+        mRecipe.setSignature(UUID.randomUUID().toString());
+        mRecipe.save();
+        Glide.with(this).load(mPhotoFile).signature(new StringSignature(mRecipe.getSignature())).into(mGlideView);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_PHOTO) {
+            updatePhotoView();
+        }
+
+    }
 }
